698e3965741012a311056d6673f5bbfa357b2870
Mr. Web Developer <117938690+Yahyobek-Coder@users.noreply.github.com>
2022-11-13 17:24:15 +0300

    Complete Responsive Suzuki Bike Site Landing Page

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+// Slider
+const slides = document.querySelectorAll('.slides');
+
+const dots = document.querySelectorAll('.dot');
+
+        function setActive(i){
+
+            for(slide of slides)
+
+            slide.classList.remove('active');
+
+            slides[i].classList.add('active');
+            
+            for(dot of dots)
+
+            dot.classList.remove('active');
+
+            dots[i].classList.add('active');
+        }
+
+            for(let j =0; j<dots.length; j++){
+                dots[j].addEventListener('click', function(){
+                    setActive(j)
+                })
+            }
+
+     // Navigation
+
+     const toggle = document.querySelector('.toggle');
+     const navigation = document.querySelector('.navigation');
+     toggle.onclick = function(){
+         toggle.classList.toggle('active');
+         navigation.classList.toggle('active');
+     }
